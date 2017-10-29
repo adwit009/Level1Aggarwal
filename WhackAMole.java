8a820cce8ac1add72f88f26df2e71ab9b0680c98
Garrett-Thomas <league@wts5.attlocal.net>
2017-10-28 17:32:15 -0700

    WackaMole

--- WhackAMole.java
+++
@@ -1,6 +0,0 @@
-
-public class WhackAMole {
-public static void main(String[] args) {
-	WhackAMole hi = new WhackAMole();
-}
-}
---
+++ src/WhackAMole.java
@@ -0,0 +1,85 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class WhackAMole implements ActionListener {
+	JPanel a;
+	JFrame b;
+	JButton mole;
+	Date timeAtStart = new Date();
+	static boolean re = true;
+	int molesWacked = 0;                                
+public static void main(String[] args) {
+	WhackAMole hi = new WhackAMole();
+hi.create();
+
+	
+}
+
+void create ()  {
+	a = new JPanel();
+	b = new JFrame();
+	b.setVisible(true);
+	b.add(a);
+	a.setLayout(new GridLayout(8,3));
+	Random rng = new Random(); 
+	int x = rng.nextInt(24);
+for (int i = 0; i < 24; i++) {
+	if (x == i) {
+		mole = new JButton("Mole!");
+		mole.addActionListener(this);
+		a.add(mole);
+	}
+	else{JButton b1 = new JButton();
+	b1.addActionListener(this);
+	b1.setSize(100, 50);
+	a.add(b1);
+	}
+}
+b.pack();
+}
+
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	if (e.getSource() == mole) {
+		speak("ow");
+		molesWacked ++;
+		b.dispose();
+if (molesWacked == 1) {
+			
+	endGame(timeAtStart, molesWacked);
+re = false;		
+}
+		
+	if ( re = true) {
+		create();
+	}	
+		
+	}
+	else {
+	speak ("no");
+	}
+}
+
+void speak(String words) {
+try {
+Runtime.getRuntime().exec("say " + words).waitFor();
+} catch (Exception e) {
+e.printStackTrace();
+}
+}
+private void endGame(Date timeAtStart, int molesWhacked) {
+Date timeAtEnd = new Date();
+JOptionPane.showMessageDialog(null, "Your whack rate is "
++ ((timeAtEnd.getTime() - timeAtStart.getTime()) / (1000.00 / molesWhacked)) + " moles per second.");
+}
+
+}
+
